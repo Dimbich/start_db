65b852eca4c21759e10f83bc51531c3fec0d419d
Dmitriy Shirmanov <shirmanovdmitriy@gmail.com>
2020-11-04 13:05:20 +0700

    Добавил трансформацию объекта в swapyservice

--- src/index.js
+++ src/index.js
@@ -1,39 +1,6 @@
-class SwapiService {
-  _apiBase = 'https://swapi.dev/api'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
-  async getResoursce(url) {
-    const res = await fetch(`${this._apiBase}${url}`);
-    if (!res.ok) {
-      throw new Error(`Не удалось получить данные с сервера ${url}. Ответ сервера ${res.status}`)
-    }
-    return await res.json();
-  
-  };
-
-  async getAllPeople() {
-    const res = await this.getResoursce(`/people/`);
-    return res.results;
-  }
-
-  getPerson(id) {
-    return this.getResoursce(`/people/${id}`);
-  }
-
-  async getAllPlanets() {
-    const res = await this.getResoursce(`/planets/`);
-    return res;
-  }
-
-  getPlanet(id) {
-    return this.getResoursce(`/planets/${id}`);
-  }
-
-  async getAllStarships() {
-    const res = await this.getResoursce(`/starships/`);
-    return res.results;
-  }
-
-  getStarship(id) {
-    return this.getResoursce(`/starships/${id}`);
-  }
-}
+ReactDOM.render(<App />,
+  document.getElementById('root'));
